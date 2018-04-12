654fc05f85ae6ab043288a72c35928c760d450d4
Stratadox <info@stratadox.com>
2018-04-12 23:56:18 +0200

    Update sortable interface

--- src/Sortable.php
+++ src/Sortable.php
@@ -2,12 +2,25 @@
 
 namespace Stratadox\Collection;
 
-interface Sortable
+use Closure;
+use Stratadox\Sorting\Contracts\DefinesHowToSort;
+
+interface Sortable extends Collection
 {
     /**
      * Sorts the values in the collection.
      *
-     * @return self|static The calling instance or an altered copy.
+     * @param Closure|null $function Optional function to decide on the order.
+     *                               Takes two items and returns -1, 0 or 1.
+     * @return self|static           The calling instance or an altered copy.
      */
-    public function sort() : self;
+    public function sort(Closure $function = null) : self;
+
+    /**
+     * Reorders the collection based on a sorting definition.
+     *
+     * @param DefinesHowToSort $sort The definition on how to sort.
+     * @return Sortable              The calling instance or an altered copy.
+     */
+    public function reorder(DefinesHowToSort $sort) : self;
 }
