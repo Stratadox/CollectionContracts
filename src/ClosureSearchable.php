4355cea6ec88be0de8618236cbcb970bd924443e
Stratadox <info@stratadox.com>
2018-04-12 22:20:55 +0200

    Add interface for searching by closure

---
+++ src/ClosureSearchable.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Stratadox\Collection;
+
+use Closure;
+
+interface ClosureSearchable
+{
+    /**
+     * Finds the first item for which the closure returns true.
+     *
+     * @param Closure $function Function that takes an item and returns boolean.
+     * @return mixed            One of the items in the collection.
+     */
+    public function findOneWith(Closure $function);
+
+    /**
+     * Checks if there is an item in the collection for which the closure
+     * returns true.
+     *
+     * @param Closure $function Function that takes an item and returns boolean.
+     * @return bool             Whether there is at least one item in the
+     *                          collection for which the function returns true.
+     */
+    public function checkWith(Closure $function): bool;
+}
