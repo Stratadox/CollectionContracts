5f82e40156ceff32c29b2d86a18194ee5a49ee1b
Stratadox <info@stratadox.com>
2018-04-12 22:16:54 +0200

    Add method to filter by closure

--- src/Filterable.php
+++ src/Filterable.php
@@ -2,6 +2,7 @@
 
 namespace Stratadox\Collection;
 
+use Closure;
 use Stratadox\Specification\Contract\Satisfiable;
 
 interface Filterable extends Collection
@@ -13,4 +14,12 @@ interface Filterable extends Collection
      * @return self|static           The calling instance or an altered copy.
      */
     public function that(Satisfiable $condition) : self;
+
+    /**
+     * Filters the collection using a specification.
+     *
+     * @param Closure $function The function that checks the elements.
+     * @return self|static      The calling instance or an altered copy.
+     */
+    public function filterWith(Closure $function) : self;
 }
