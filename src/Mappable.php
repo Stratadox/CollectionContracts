78f89d5a1979a4f44c3a5bb28a639073e63bf9e9
Stratadox <info@stratadox.com>
2020-11-02 18:55:19 +0100

    Mapping a collection doesn't necessarily result in the same kind of collection

--- src/Mappable.php
+++ src/Mappable.php
@@ -10,7 +10,7 @@ interface Mappable extends Collection
      * Applies the function to each of the elements, putting back the result.
      *
      * @param Closure $function The function used for mapping the collection.
-     * @return static           The calling instance or an altered copy.
+     * @return Collection       The mapped collection.
      */
-    public function map(Closure $function);
+    public function map(Closure $function): Collection;
 }
